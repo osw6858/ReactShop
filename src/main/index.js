05e1d3ae21874f3603ccc699b0b7eafc494cd69a
osw6858 <osw7875@naver.com>
2022-12-14 15:46:41 +0900

    반응형 웹 구현

--- src/main/index.js
+++ src/main/index.js
@@ -3,13 +3,14 @@ import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
+import { Carousel } from "antd";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { API_URL } from "../config/constant.js";
 dayjs.extend(relativeTime);
 
 function MainPageComponent() {
   const [products, setProduts] = React.useState([]);
-
+  const [banners, setBanners] = React.useState([]);
   React.useEffect(function () {
     axios
       //useEffect가 없으면 무한루프
@@ -21,18 +22,37 @@ function MainPageComponent() {
       .catch(function (error) {
         console.log("에러발생  :", error);
       });
+
+    axios
+      .get(`${API_URL}/banners`)
+      .then((result) => {
+        const banners = result.data.banners;
+        setBanners(banners);
+      })
+      .catch((error) => {
+        console.error("에러 발생 : ", error);
+      });
   }, []);
 
   return (
     <div>
-      <div id="banner">
-        <img src="images/banners/banner1.png" alt="배너이미지" />
-      </div>
+      <Carousel autoplay autoplaySpeed={3000}>
+        {banners.map((banner, index) => {
+          return (
+            <Link to={banner.href}>
+              <div id="banner">
+                <img src={`${API_URL}/${banner.imageUrl}`} />
+              </div>
+            </Link>
+          );
+        })}
+      </Carousel>
       <h1 id="product-headline">판매되는 상품들</h1>
       <div id="product-list">
         {products.map((product, index) => {
           return (
             <div className="product-card">
+              {product.soldout === 1 && <div className="product-blur" />}
               <Link className="product-link" to={`/products/${product.id}`}>
                 <div>
                   <img
--- src/product/index.js
+++ src/product/index.js
@@ -6,16 +6,17 @@ import { Space, Spin } from "antd";
 import "./index.css";
 import dayjs from "dayjs";
 import { API_URL } from "../config/constant.js";
+import { Button, message } from "antd";
 
 function PorductComponent() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
 
-  useEffect(function () {
+  const getProduct = () => {
     axios
       .get(
         `
-        ${API_URL}/products/${id}`
+      ${API_URL}/products/${id}`
       )
       .then(function (result) {
         setProduct(result.data.product);
@@ -23,7 +24,23 @@ function PorductComponent() {
       .catch(function (error) {
         console.error(error);
       });
+  };
+
+  useEffect(function () {
+    getProduct();
   }, []);
+
+  const onClickPurchase = () => {
+    axios
+      .post(`${API_URL}/purchase/${id}`)
+      .then((result) => {
+        message.info("구매가 완료되었습니다");
+        getProduct();
+      })
+      .catch((error) => {
+        message.error(`에러가 발생했습니다. ${error.message}`);
+      });
+  };
 
   /*비동기 통신으로 페이지가 렌더링이 먼저 되고 promise객체가 나중에 업데이트 되면서 렌더링되는데
     useState 초기값이 null이라 첫 렌더링시 null.상품이름 하면 오류가 나기 때문에 조건문 설정*/
@@ -52,6 +69,16 @@ function PorductComponent() {
         <div id="createdAt">
           {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
         </div>
+        <Button
+          id="purchase-button"
+          size="large"
+          type="primary"
+          danger
+          onClick={onClickPurchase}
+          disabled={product.soldout === 1}
+        >
+          바로 구매하기
+        </Button>
         <pre id="description">{product.description}</pre>
       </div>
     </div>
